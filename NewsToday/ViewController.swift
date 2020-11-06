91cfdd71493a9788031da3ff24a5ae32376c2e42
ValeraRoman <valeraromanlaslo@gmail.com>
2020-11-06 16:53:17 +0200

    Initial Commit

---
+++ NewsToday/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToday
+//
+//  Created by Macbook Air 13 on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
